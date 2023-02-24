f181dc59ffde45db8dd34b8bacd07f7791bbd248
Knielsza <piontek.milosz@gmail.com>
2023-02-24 10:39:30 +0100

    nice

--- server01.js
+++ server01.js
@@ -1,9 +1,11 @@
 
-const { setMaxListeners } = require("events");
+
 const express = require("express")
 const app = express()
 const PORT = 3000;
 
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.get("/", function (req, res) {
 //     res.send("dane html odesłane z serwera do przeglądarki")
@@ -22,55 +24,64 @@ app.get("/", function (req, res) {
 })
 
 app.post("/handleForm", function (req, res) {
-    console.log(req.query.first)
-    console.log(req.query.second)
-    console.log(req.query.whatmath)
+
+
+    console.log(req.body);
+    var first1 = parseInt(req.body.first)
+    var second1 = parseInt(req.body.second)
+    let math = (req.body.whatmath)
+
+    console.log(first1)
+    console.log(second1)
+    console.log(math)
     // res.send("kkk")
 
     // var whatmath = Number(req.body.select.whatmath)
     // console.log(whatmath)
-    var first1 = Number(req.query.first)
-    var second1 = Number(req.query.second)
+    // res.send("wynik:" + first1)
 
-    res.send("wynik:" + first1)
     // res.send(first1 + second1)
-
-    // var finalr = first1 - second1
-    // var finalm = first1 * second1
-    // var finald = first1 / second1
+    var finals = first1 - second1
+    var finalr = first1 - second1
+    var finalm = first1 * second1
+    var finald = first1 / second1
     console.log("wynik")
     // console.log(first1)
     // console.log(second1)
-    // var data = []
-    // switch (whatmath) {
-    //     case "Suma":
-    //         data[0] = { message: req.body.whatmath + "dwu liczb", wynik: finals }
-    //         break
-    //     case "Różnica":
-    //         data[0] = { message: req.body.whatmath + "dwu liczb", wynik: finalr }
-    //         break
-    //     case "Iloczyn":
-    //         data[0] = { message: req.body.whatmath + "dwu liczb", wynik: finalm }
-    //         break
-    //     case "Iloraz":
-    //         data[0] = { message: req.body.whatmath + "dwu liczb", wynik: finald }
-    //         break
-    //     case "All":
-    //         // console.log(finals)
-    //         // console.log(finalr)
-    //         // console.log(finalm)
-    //         // console.log(finald)
-    //         break;
-    // }
+    var data = []
     res.header("content-type", "application/json")
-    res.send(JSON.stringify("wynik:" + finals))
-    // res.send("wynik:" + finals)
-    // res.send("wynik:" + finalr)
-    // res.send("wynik:" + finalm)
-    // res.send("wynik:" + finald)
 
-    // res.header("content-type", "application/json")
-    // JSON.stringify(data, null, 5)
+    switch (math) {
+        case "Suma":
+            data[0] = { message: req.body.whatmath + " dwu liczb", wynik: finals }
+            break
+        case "Różnica":
+            data[0] = { message: req.body.whatmath + " dwu liczb", wynik: finalr }
+            break
+        case "Iloczyn":
+            data[0] = { message: req.body.whatmath + " dwu liczb", wynik: finalm }
+            break
+        case "Iloraz":
+            data[0] = { message: req.body.whatmath + " dwu liczb", wynik: finald }
+            break
+        case "All":
+            // console.log(finals)
+            // console.log(finalr)
+            // console.log(finalm)
+            // console.log(finald)
+            break;
+    }
+    // res.send(JSON.stringify("wynik:" + finals))
+    // // res.send("wynik:" + finals)
+    // // res.send("wynik:" + finalr)
+    // // res.send("wynik:" + finalm)
+    // // res.send("wynik:" + finald)
 
+    // // res.header("content-type", "application/json")
+
+    // app.use(bodyParser.json())
+    res.send(data[0])
+
+    JSON.stringify(data, null, 5)
 
 })
